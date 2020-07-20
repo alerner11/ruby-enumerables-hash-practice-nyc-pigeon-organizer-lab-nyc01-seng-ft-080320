d136bc7e31f657cc7ccc61baa9febce02613a91e
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-20 19:40:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key1, key2|
+    data[key1].each do |key2, array| 
+      array.select do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][key1]
+          pigeon_list[name][key1] = []
+        end
+        pigeon_list[name][key1] << key2.to_s
+      end
+    end
+  end
+  pigeon_list
 end
